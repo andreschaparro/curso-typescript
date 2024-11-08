d7d4bd98390c866920e76f79c5dad2ea7ef07d5a
andreschaparro <chaparroandres87@gmail.com>
2024-11-08 10:27:24 -0300

    Tipado de Objetos con Interfaces

---
+++ 7_tipado _de_objetos_con_interfaces/script.ts
@@ -0,0 +1,19 @@
+interface Programador {
+    nombre: string,
+    tecnologias: string[],
+    homeOffice?: boolean | null
+}
+
+let programador1: Programador = {
+    nombre: 'José Pérez',
+    tecnologias: ['C', 'C++', 'Arduino'],
+    homeOffice: false
+}
+
+let programador2: Programador = {
+    nombre: 'Josefina Paredes',
+    tecnologias: ['HTML', 'CSS', 'JS', 'REACT'],
+}
+
+console.log(programador1);
+console.log(programador2);
