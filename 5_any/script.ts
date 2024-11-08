662416ded5ffbf62b60bc74d2ca337a3280197d6
andreschaparro <chaparroandres87@gmail.com>
2024-11-08 09:52:16 -0300

    any

---
+++ 5_any/script.ts
@@ -0,0 +1,23 @@
+// En los siguientes casos, TS infiere el tipo de dato de las variables en función de los valores que se les asignan
+// Conclusión: nunca hacer lo que se muestra a continuación
+let variable;
+
+variable = 'Cadena de texto 1';
+console.log(variable);
+
+variable = 1000;
+console.log(variable);
+
+variable = true;
+console.log(variable);
+
+let variableAny: any;
+
+variableAny = 'Cadena de texto 2';
+console.log(variableAny);
+
+variableAny = 2000;
+console.log(variableAny);
+
+variableAny = false;
+console.log(variableAny);
